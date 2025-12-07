0ecca1a1cfa49253d2f4e03565b0eb375cb002dd
Ahmad Duwi Setianto <setiantoahmad02@gmail.com>
2025-12-07 13:51:25 +0700

    Ganti nama favicon jadi logo-baru

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel - DentalCare Expert</title>
-    <link rel="shortcut icon" href="{{ asset('images/logo-baru.png') }}" type="image/png">
+    <link rel="shortcut icon" href="/images/logo-baru.png" type="image/png">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Font Awesome -->
